1b42c4e2bea884965f8d6f21f4bc948bede771ec
PrincyChauhan <chauhanprincee7@gmail.com>
2025-04-09 14:53:23 +0530

    form elements

--- src/components/OnboardingPage.jsx
+++ src/components/OnboardingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Onboard from "./Onboard";
 import ClientRequirement from "./ClientRequirement";
+import FormElement from "./FormElement";
 
 const OnboardingPage = () => {
   return (
     <div className="flex gap-6 bg-[#F9FAFB]">
       <Onboard />
       <ClientRequirement />
+      <FormElement />
     </div>
   );
 };
