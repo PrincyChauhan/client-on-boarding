17b83587b2ce0151381643ed80dba13cdd82f36f
Princy Chauhan <66620521+PrincyChauhan@users.noreply.github.com>
2025-04-29 14:59:53 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="flex items-center justify-between px-4 py-2 border border-[#EAECF0] bg-white rounded-md">
         <div className="flex items-center">
           <img src={logo} alt="Logo" className="w-8 h-8 mr-2" />
-          <h1 className="text-lg font-semibold text-gray-800">Untitled UI</h1>
+          <h1 className="text-lg font-semibold text-gray-800">ATS</h1>
         </div>
         <div className="w-10 h-10 flex items-center justify-center rounded-md">
           <img src={bell} alt="Bell" className="w-8 h-8 mr-2" />
